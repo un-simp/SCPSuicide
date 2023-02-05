1f69f368d425a013b7208922c557d82d4323bacb
warden161 <118567564+warden161@users.noreply.github.com>
2023-02-05 14:36:47 -0500

    versioning

--- SuicidePro.Addons/Plugin.cs
+++ SuicidePro.Addons/Plugin.cs
@@ -7,7 +7,7 @@ namespace SuicidePro.Addons
     {
         public override string Name { get; } = "SuicidePro.Addons";
         public override string Author { get; } = "warden161";
-        public override Version Version { get; } = new Version(0, 1, 0);
+        public override Version Version { get; } = new Version(1, 0, 0);
         public override Version RequiredExiledVersion { get; } = new Version(6, 0, 0);
 
         public static Plugin Instance { get; private set; }
