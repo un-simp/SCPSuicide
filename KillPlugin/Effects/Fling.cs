e4bf51227b1478a4431d446df028085277c23dd5
un-simp <unsimp@billgates.sex>
2024-10-23 21:55:59 +0100

    FIXED THAT FLING BUG I FORGOT TO ENABLE UNSAFE CODE

--- KillPlugin/Effects/Fling.cs
+++ KillPlugin/Effects/Fling.cs
@@ -1,27 +1,23 @@
-
+using System.Reflection;
 using Exiled.API.Features;
 using KillPlugin.Interfaces;
 using PlayerStatsSystem;
-using UnityEngine;
-
-// base new() {Name = "fling",Aliases = new[] {"wee"},Description = "Weeeeeeeeeeeeee",
-// Response = "tripping", Reason = "Tripped!", Velocity = new Velocity(15, 1, 0)},
-
-
 
 namespace KillPlugin.Effects
 {
     public class Fling : IDeathEffect
     {
         public string Name { get; } = "fling";
-        public string Description { get; } = "Go WEEEEEEEEEEEE";
+        public string Description { get; } = "Flings the player into the air";
+
         public bool Run(Player player)
         {
-            CustomReasonDamageHandler customDeath = new CustomReasonDamageHandler("went weeeee", -1, string.Empty);
-            Vector3 velocity = new Velocity(15, 1, 0).ToVector3(player.Transform);
-            customDeath.StartVelocity = velocity;
-            player.Kill(customDeath);
+            var death = new CustomReasonDamageHandler("Went Weeeeeeeeeeeeeeee", -1, string.Empty);
+            var velocity = new Velocity(0, 10, 0).ToVector3(player.Transform);
+            death.StartVelocity = velocity;
+            player.Kill(death);
             return true;
+            
         }
     }
 }
--- KillPlugin/Plugin.cs
+++ KillPlugin/Plugin.cs
@@ -7,7 +7,7 @@ namespace KillPlugin
         public override string Author { get; } = "Jeremy SCP";
         public override string Name { get; } = "kill-plugin";
         public override Version Version { get; } = new(3, 4, 0);
-        public override Version RequiredExiledVersion { get; } = new(8, 2, 1);
+        public override Version RequiredExiledVersion { get; } = new(8, 7, 0);
 
         public static Plugin Instance;
 
